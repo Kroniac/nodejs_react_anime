2d788e61a8612b2d001fe71121684118d2755ea6
Farid Ansari <farid12ansari7@gmail.com>
2018-12-10 02:15:33 +0530

    added POST as allowed methods, added Allow-Headers and added body parser to parse the incoming requests

--- server_nodejs/index.js
+++ server_nodejs/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const feedRoutes = require('./routes/feed');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
