0854abc53207d64c913cd5a5bbe4455c6fa241a6
Farid Ansari <farid12ansari7@gmail.com>
2019-01-27 18:44:39 +0530

    updated anime characters detais slider design

--- client_react/src/scenes/anime_characters/anime_characters.js
+++ client_react/src/scenes/anime_characters/anime_characters.js
@@ -8,13 +8,11 @@ class AnimeCharacters extends Component {
   constructor(props) {
     super(props);
     this.images = [
-      "https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg",
-      "https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg",
-      "https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg",
-      "https://upload.wikimedia.org/wikipedia/en/thumb/6/65/The_Rising_of_the_Shield_Hero_light_novel_vol_1.jpg/220px-The_Rising_of_the_Shield_Hero_light_novel_vol_1.jpg",
-      "https://images-eu.ssl-images-amazon.com/images/I/61nZiU%2BI98L._SY445_QL70_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/I/51uxJBQfGXL._SY346_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/I/518Mvgn2cCL._SX339_BO1,204,203,200_.jpg",
+      require('../../images/goku1.png'),
+      require('../../images/vegeta.png'),
+      require('../../images/master_roshi.png'),
+      require('../../images/goku_kid.png'),
+      require('../../images/freeza.png')
     ];
     this.state = {
       currentImage: 0,
@@ -23,56 +21,63 @@ class AnimeCharacters extends Component {
       hideLeft: Styles.hideLeft,
       next: Styles.next,
       hideRight: Styles.hideRight,
-      image1: this.images[0],
-      image2: this.images[1],
-      image3: this.images[2],
-      image4: this.images[3],
-      image5: this.images[4]
+      image1: null,
+      image2: null,
+      image3: this.images[0],
+      image4: this.images[1],
+      image5: this.images[2]
     }
   }
   
 
   _prevCard = () => {
-    this.currentHideLeftPosition = (this.currentHideLeftPosition + 1) % 6 || 1;
-    this.currentHideRightPosition = (this.currentHideRightPosition + 1) % 6 || 1;
-    this.setState((prevState) => ({
-      prev: this.nextPressed[prevState.prev],
-      selected: this.nextPressed[prevState.selected],
-      hideLeft: this.nextPressed[prevState.hideLeft],
-      next: this.nextPressed[prevState.next],
-      hideRight: this.nextPressed[prevState.hideRight],
-      currentImage: 1
-    }), () => {
-      ['hideLeft', 'prev', 'selected', 'next', 'hideRight'].forEach((current, index) => {
-        if (this.state[current] === Styles.hideLeft) this.setState({ [`image${index+1}`]: this.images[this.prevImage - 1] })
-      })
-      this.prevImage -= 1;
-      this.nextImage -= 1;
-    });
+    if (this.selectedImageIndex === 0) console.log('No Prev Images');
+    else {
+      this.currentHideLeftPosition = (this.currentHideLeftPosition + 1) % 6 || 1;
+      this.currentHideRightPosition = (this.currentHideRightPosition + 1) % 6 || 1;
+      this.selectedImageIndex -= 1;
+      this.setState((prevState) => ({
+        prev: this.nextPressed[prevState.prev],
+        selected: this.nextPressed[prevState.selected],
+        hideLeft: this.nextPressed[prevState.hideLeft],
+        next: this.nextPressed[prevState.next],
+        hideRight: this.nextPressed[prevState.hideRight],
+      }), () => {
+        ['hideLeft', 'prev', 'selected', 'next', 'hideRight'].forEach((current, index) => {
+          if (this.state[current] === Styles.hideLeft) this.setState({ [`image${index+1}`]: this.images[this.prevImage - 1] })
+        });
+        this.prevImage -= 1;
+        this.nextImage -= 1;
+      });
+    }
   }
 
-  prevImage = 0;
-  nextImage = 4;
+  selectedImageIndex = 0;
+  prevImage = -2;
+  nextImage = 2;
   currentHideLeftPosition = 1;
   currentHideRightPosition = 5;
 
   _nextCard = () => {
-    this.currentHideLeftPosition = (this.currentHideLeftPosition - 1) || 5;
-    this.currentHideRightPosition = (this.currentHideRightPosition - 1) || 5;
-    this.setState((prevState) => ({
-      prev: this.prevPressed[prevState.prev],
-      selected: this.prevPressed[prevState.selected],
-      hideLeft: this.prevPressed[prevState.hideLeft],
-      next: this.prevPressed[prevState.next],
-      hideRight: this.prevPressed[prevState.hideRight],
-      currentImage: 1
-    }), () => {
-      ['hideLeft', 'prev', 'selected', 'next', 'hideRight'].forEach((current, index) => {
-        if (this.state[current] === Styles.hideRight) this.setState({ [`image${index+1}`]: this.images[this.nextImage + 1] })
-      })
-      this.prevImage += 1;
-      this.nextImage += 1;
-    });
+    console.log(this.selectedImageIndex)
+    if (this.selectedImageIndex + 1 < this.images.length) {
+      this.currentHideLeftPosition = (this.currentHideLeftPosition - 1) || 5;
+      this.currentHideRightPosition = (this.currentHideRightPosition - 1) || 5;
+      this.selectedImageIndex += 1;
+      this.setState((prevState) => ({
+        prev: this.prevPressed[prevState.prev],
+        selected: this.prevPressed[prevState.selected],
+        hideLeft: this.prevPressed[prevState.hideLeft],
+        next: this.prevPressed[prevState.next],
+        hideRight: this.prevPressed[prevState.hideRight],
+      }), () => {
+        ['hideLeft', 'prev', 'selected', 'next', 'hideRight'].forEach((current, index) => {
+          if (this.state[current] === Styles.hideRight) this.setState({ [`image${index+1}`]: this.images[this.nextImage + 1] })
+        })
+        this.prevImage += 1;
+        this.nextImage += 1;
+      });
+    } else console.log('No next images');
   }
 
   nextPressed = {
@@ -98,20 +103,32 @@ class AnimeCharacters extends Component {
       <div className = {Styles.root} >
         <div className = {Styles.carasoul} >
         <div className={this.state.hideLeft}>
+        <div>
         <img src={this.state.image1} />
         </div>
+        
+        </div>
         <div className = {this.state.prev}>
+        <div>
         <img src={this.state.image2} />
+        </div>
       </div>
       
       <div class = {this.state.selected}>
+      <div>
         <img src={this.state.image3} />
+        </div>
+        <span>GOKU</span>
       </div>
       <div className = {this.state.next}>
+        <div>
         <img src={this.state.image4} />
+        </div>
       </div>
       <div className = {this.state.hideRight}>
+      <div>
         <img src={this.state.image5} />
+      </div>
       </div>
       </div>
       <div className = {Styles.buttons} >
@@ -123,6 +140,7 @@ class AnimeCharacters extends Component {
       </Button>
       </div>
       </div>
+
     )
   }
 }
