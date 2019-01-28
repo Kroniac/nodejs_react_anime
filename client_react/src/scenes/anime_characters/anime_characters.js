bc8c11199a96fb0a869f790e493fe8f03a426949
Farid Ansari <farid12ansari7@gmail.com>
2019-01-29 00:35:20 +0530

    added characters title and details area

--- client_react/src/scenes/anime_characters/anime_characters.js
+++ client_react/src/scenes/anime_characters/anime_characters.js
@@ -8,11 +8,46 @@ class AnimeCharacters extends Component {
   constructor(props) {
     super(props);
     this.images = [
-      require('../../images/goku1.png'),
-      require('../../images/vegeta.png'),
-      require('../../images/master_roshi.png'),
-      require('../../images/goku_kid.png'),
-      require('../../images/freeza.png')
+      {
+        background_color: 'linear-gradient(to top right, red , orange)',
+        image: require('../../images/goku1.png'),
+        title: 'Son Goku',
+        detail: 'Goku is a Saiyan',
+      },
+      {
+        background_color: 'linear-gradient(to top right, blue , lightblue)',
+        image: require('../../images/vegeta.png'),
+        title: 'Vegeta',
+        detail: 'Goku is a Saiyan',
+      },
+      {
+        background_color: 'linear-gradient(to top right, red , orange)',
+        image: require('../../images/master_roshi.png'),
+        title: 'Master Roshi',
+        detail: 'Goku is a Saiyan',
+      },
+      {
+        background_color: 'linear-gradient(to top right, orange , yellow)',
+        image: require('../../images/goku_kid.png'),
+        title: 'Kid Goku',
+        detail: 'Goku is a Saiyan',
+      },
+      {
+        background_color: 'linear-gradient(to top right, purple , orchid)',
+        image: require('../../images/freeza.png'),
+        title: 'Freeza',
+        detail: 'Goku is a Saiyan',
+      },
+      {
+        background_color: 'linear-gradient(to top right, green , lightgreen)',
+        image: require('../../images/broly.png'),
+        title: 'Broly',
+        detail: 'Goku is a Saiyan',
+      },
+      
+      
+      
+      
     ];
     this.state = {
       currentImage: 0,
@@ -99,37 +134,115 @@ class AnimeCharacters extends Component {
 
 
   render() {
+    const { image1, image2, image3, image4, image5 } = this.state;
     return (
       <div className = {Styles.root} >
         <div className = {Styles.carasoul} >
-        <div className={this.state.hideLeft}>
-        <div>
-        <img src={this.state.image1} />
-        </div>
-        
-        </div>
-        <div className = {this.state.prev}>
-        <div>
-        <img src={this.state.image2} />
-        </div>
-      </div>
-      
-      <div class = {this.state.selected}>
-      <div>
-        <img src={this.state.image3} />
-        </div>
-        <span>GOKU</span>
-      </div>
-      <div className = {this.state.next}>
-        <div>
-        <img src={this.state.image4} />
-        </div>
-      </div>
-      <div className = {this.state.hideRight}>
-      <div>
-        <img src={this.state.image5} />
-      </div>
-      </div>
+        {
+          image1 ? (
+            <div style = {{ backgroundImage: image1.background_color }} className={this.state.hideLeft}>
+              <span className = {Styles.cardHeaderSection} >
+              <span className = {Styles.imager} >
+                <img src={this.state.image1.image} />
+              </span>
+              <div>
+              </div>
+              <span
+                className = {Styles.cardTitle}
+              >
+                {image1.title}
+              </span>
+              </span>
+              <span className = {Styles.cardDetailText} >
+                {image1.detail}
+              </span>
+            </div>
+          ) : null
+        }
+        {
+          image2 ? (
+            <div style = {{ backgroundImage: image2.background_color }} className = {this.state.prev}>
+              <span className = {Styles.cardHeaderSection} >
+              <span className = {Styles.imager} >
+                <img src={this.state.image2.image} />
+              </span>
+              <div>
+              </div>
+              <span
+                className = {Styles.cardTitle}
+              >
+                {image2.title}
+              </span>
+              </span>
+              <span className = {Styles.cardDetailText} >
+                {image2.detail}
+              </span>
+            </div>
+          ) : null
+        }
+        {
+          image3 ? (
+            <div style = {{ backgroundImage: image3.background_color }} className = {this.state.selected}>
+            <span className = {Styles.cardHeaderSection} >
+              <span className = {Styles.imager} >
+                <img src={this.state.image3.image} />
+              </span>
+              <div>
+              </div>
+              <span
+                className = {Styles.cardTitle}
+              >
+                {image3.title}
+              </span>
+              </span>
+              <span className = {Styles.cardDetailText} >
+                {image3.detail}
+              </span>
+            </div>
+          ) : null
+        }
+        {
+          image4 ? (
+            <div style = {{ backgroundImage: image4.background_color }} className = {this.state.next}>
+                <span className = {Styles.cardHeaderSection} >
+              <span className = {Styles.imager} >
+                <img src={this.state.image4.image} />
+              </span>
+              <div>
+              </div>
+              <span
+                className = {Styles.cardTitle}
+              >
+                {image4.title}
+              </span>
+              </span>
+              <span className = {Styles.cardDetailText} >
+                {image4.detail}
+              </span>
+            </div>
+          ) : null
+        }
+        {
+          image5 ? (
+            <div style = {{ backgroundImage: image5.background_color }} className = {this.state.hideRight}>
+              <span className = {Styles.cardHeaderSection} >
+              <span className = {Styles.imager} >
+                <img src={this.state.image5.image} />
+              </span>
+              <div>
+              </div>
+              <span
+                className = {Styles.cardTitle}
+              >
+                {image5.title}
+              </span>
+              </span>
+              <span className = {Styles.cardDetailText} >
+                {image5.detail}
+              </span>
+            </div>
+          ) : null
+        }
       <div className = {Styles.buttons} >
       <Button mode="raised" design="accent" onClick={this._prevCard}>
           Prev
